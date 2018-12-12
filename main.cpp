2ba3f9ca3c6ebd1919eb529741d1c72b8b5b1692
mustiian <ianmustiats@seznam.cz>
2018-12-12 15:04:29 +0100

    Get a little bit forward. Implemented GetAllWords and InsertWordToSong methods, with comments

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,8 +15,8 @@ public:
     void InsertNeighbour (int neighbour, char ton){
         m_Neighbours.insert( {neighbour, ton} );
     }
-    char GetNeighbourTon( int neighbour ){
-        return m_Neighbours.at(neighbour);
+    map<int, char>& GetNeighbours(){
+        return m_Neighbours;
     }
 
 private:
@@ -33,6 +34,8 @@ public:
         }
     }
     void InsertNeighbour( int position, int neighbour, char ton );
+    void GetAllWords ( int startPosition );
+    void InsertWordToSong( int position, const string& word, char ton );
 private:
     vector<TTon> m_Song;
     map<int, set<string>> m_Words;
@@ -42,6 +45,28 @@ private:
 
 void CKoledy::InsertNeighbour(int position, int neighbour, char ton) {
     m_Song[position].InsertNeighbour(neighbour, ton);
+}
+
+void CKoledy::GetAllWords(int position) {
+    if ( position == m_End ){                                               /// IF THE LAST POSITION:
+        m_Words.at(m_End).insert("");                                       /// INSERT NOTHING
+    }
+    for ( const auto& neighbour: m_Song[position].GetNeighbours() ){        /// FOR ALL NEIGHBOURS:
+        int neighbourPosition = neighbour.first;
+        char neighbourTon = neighbour.second;
+
+        if ( m_Words.find( neighbourPosition ) != m_Words.end() ){          /// IF NEIGHBOUR HAS ALL WORDS:
+            for ( const string& word: m_Words.at( neighbourPosition ) )     /// FOR ALL WORDS:
+                InsertWordToSong(position, word, neighbourTon);             /// INSERT WORD WITH NEW CHAR IN THIS POSITION
+        }
+        else {                                                              /// ELSE NEIGHBOUR FIND
+            GetAllWords(neighbourPosition);                                 /// ALL HIS NEIGHBOURS WORDS
+        }
+    }
+}
+
+void CKoledy::InsertWordToSong(int position, const string &word, char ton) {
+    m_Words.at(position).insert(ton + word);
 }
 
 int main(){
